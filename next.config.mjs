57c115844c0e012d51171bc7d8837e34b35a80bf
john Michael Panganiban <johnmichaelpanganiban.its@gmail.com>
2025-03-07 11:46:12 +0800

    remove export and use output

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+module.exports = {
+    output: 'export',  // Set the output to export static files
+    basePath: '/nexus-erp',  // Optional: Set the base path for GitHub Pages
+    trailingSlash: true,  // Optional: Add trailing slashes for static URLs
+};
+  
 
-export default nextConfig;
+// export default nextConfig;
